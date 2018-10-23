d0a4ad9b55c0fe2c7ae7dd1922f00965cd409c1b
zpcsgo <15229065699@163.com>
2018-10-23 22:32:25 +0800

    first commit mysmallprogect

---
+++ pages/index/index.js
@@ -0,0 +1,80 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    imageUrl: [{
+        url: "../images/1.jpg"
+      },
+      {
+        url: "../images/2.jpg"
+      },
+      {
+        url: "../images/3.jpg"
+      },
+      {
+        url: "../images/4.jpg"
+      },
+      {
+        url: "../images/5.jpg"
+      },
+      {
+        url: "../images/6.jpg"
+      },
+      {
+        url: "../images/7.jpg"
+      },
+    ]
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  Test: function() {
+
+    console.log("测试点击");
+  },
+  onLoad: function() {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
